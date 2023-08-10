61f152260b53ee24f342ea2a1ec4d0ba64b4f327
P.W.Geethika Isuru Sampath <35458073+geethikaisuru@users.noreply.github.com>
2023-08-10 20:24:45 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      black: "#000000",
+      white: "#ffffff",
+      blue: "#347AE2",
+      "light-blue": "#DFEAF7",
+      "lighter-blue": "#EBF3FD",
+      "gray-F3F2EF": "#F3F2EF",
+      "gray-858EAD": "#858EAD",
+      "gray-424242": "#424242",
+      "gray-F4F6F8": "#F4F6F8",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
